6662d19c9af8ee5cc20451f9df3d3f28134a7fd1
Priyank Thakur <priyankthakur3@live.com>
2024-02-10 17:30:43 -0500

    footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2024 Priyank Thakur. All Rights reserved
+      </small>
+      <p className="font-semibold text-xs">
+        <span>About this website:</span> Built with React.js, Next.js,
+        TypeScript, Tailwind CSS, Framer Motion. Hosted on Vercel
+      </p>
+    </footer>
+  );
+}
