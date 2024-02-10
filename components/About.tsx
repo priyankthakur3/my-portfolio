6b3a7312f7a92d127f3dd2cb61c24a25dfa48413
Priyank Thakur <priyankthakur3@live.com>
2024-02-09 19:26:02 -0500

    fixes

--- components/About.tsx
+++ components/About.tsx
@@ -10,11 +10,11 @@ export default function About() {
   return (
     <motion.section
       ref={ref}
+      id="about"
       className="mb-20 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-[100rem]"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
-      id="about"
     >
       <SectionHeader>About Me</SectionHeader>
       <p className="mb-3">
